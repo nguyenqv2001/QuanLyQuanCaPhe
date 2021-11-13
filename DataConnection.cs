0e2ff70d9e501b54398a5c49db5906f0ad3fc981
johnkater <nhatkater130@gmail.com>
2021-11-13 19:26:47 +0700

    Add new file

---
+++ DataConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace QuanLyQuanCaPhe
+{
+    class DataConnection
+    {
+        string conStr;
+        public DataConnection()
+        {
+            conStr = "Data Source=DESKTOP-6V6IGLT\\SQLEXPRESS;Initial Catalog=QuanLyNhanSu;Integrated Security=True";
+
+        }
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(conStr);
+        }
+
+
+    }
+}
