fdf208a2c63b5a0f210abfef298b069f08e47a01
johnkater <nhatkater130@gmail.com>
2021-11-17 13:18:55 +0700

    change form6

--- Form6.Designer.cs
+++ Form6.Designer.cs
@@ -32,7 +32,7 @@ namespace QuanLyQuanCaPhe
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form6";
+            this.Text = "Form Quan Ly nhan vien";
         }
 
         #endregion
