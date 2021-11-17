c41eb9035b8740e473791f0b38268953e527aa3f
johnkater <nhatkater130@gmail.com>
2021-11-17 11:47:48 +0700

    test thay doi

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ namespace QuanLyQuanCaPhe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form4());
+            Application.Run(new Form1());
         }
     }
 }
